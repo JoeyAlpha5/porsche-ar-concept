8340f01c26fb85b391bf0f6afeda8399d9ed2f2a
Jalome Chirwa <click2sure@Jalomes-Air.home>
2022-10-19 23:35:06 +0200

    initial upload

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# porsche-ar-concept
+
+
+**App Screenshots**
+<img src='./screenshot.png' width='250px' />
---
+++ porsche930Turbo/porsche930Turbo/CarHighlight.swift
@@ -0,0 +1,10 @@
+
+class CarHiglight{
+    var highlightTitle: String
+    var text: String
+    
+    init(highlightTitle:String, text: String){
+        self.highlightTitle = highlightTitle
+        self.text = text
+    }
+}
